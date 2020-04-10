417e45fdeb9963301222fac2ae8d83eb05547257
Chirag Agarwal <cagarwal@chirag.centos>
2020-04-10 20:35:40 +0100

    Updates

---
+++ precache-manifest.f942f06a8aa1a738670d718ed1cd42d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8978a686f6fb4a10f20b520ed8fe82a4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5e1114e1afb370bfd2cc",
+    "url": "/robofriends/static/css/main.352a9c16.chunk.css"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1114e1afb370bfd2cc",
+    "url": "/robofriends/static/js/main.0b835843.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  },
+  {
+    "revision": "354ba80e366abe5cafa64de875e49237",
+    "url": "/robofriends/static/media/ostrich-sans-bold.354ba80e.ttf"
+  },
+  {
+    "revision": "841856237f1c5e80132317b789cec6f0",
+    "url": "/robofriends/static/media/ostrich-sans-bold.84185623.woff"
+  },
+  {
+    "revision": "e5eada6f8f26323ccf7b2cfaf4025ad2",
+    "url": "/robofriends/static/media/ostrich-sans-bold.e5eada6f.svg"
+  },
+  {
+    "revision": "f4f68ce55eebab8d803447135e693765",
+    "url": "/robofriends/static/media/ostrich-sans-bold.f4f68ce5.eot"
+  }
+]);
